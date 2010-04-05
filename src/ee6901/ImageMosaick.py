d350197b5c7a32c08a34450d79d14a87d8b28a27
songuke <songuke@8f160610-0e58-120a-f56f-8676c18f249f>
2010-04-05 10:17:48 +0000

    RANSAC complete.

--- src/ee6901/ImageMosaick.py
+++ src/ee6901/ImageMosaick.py
@@ -3,7 +3,10 @@ Created on Mar 31, 2010
 @author: Hua Binh Son
 '''
 
+import os
 import numpy as np
+from scipy import linalg
+import scipy as sp
 import pylab
 import sift
 import KdtreeCustom
@@ -15,8 +18,8 @@ class Feature(object):
         self.descriptor = descriptor;
         self.location   = location;
 
-def featureDistance(f1, f2):
-    return 1.0 - abs(numpy.dot(f1.descriptor, f2.descriptor));
+#def featureDistance(f1, f2):
+    #return 1.0 - abs(numpy.dot(f1.descriptor, f2.descriptor));
         
 class Image(object):
     '''
@@ -58,7 +61,10 @@ class ImageManager(object):
             # SIFT features
             partName, partDot, partExt = file.rpartition('.');
             keyFile = ''.join(partName + partDot + ("key")); # join tuples to string
-            sift.process_image(file, keyFile);
+            
+            if os.path.exists(keyFile) == False:            
+                sift.process_image(file, keyFile);
+                
             loc, des  = sift.read_features_from_file(keyFile);
             #im.features = [Feature(im.id, des[i], loc[i]) for i in range(len(des))];
             #print "Total features: ", len(im.features)
@@ -85,7 +91,104 @@ class ImageMatch(object):
         self.image2 = None;
         self.locs1 = [];
         self.locs2 = [];
-    
+        
+    def __homography__(self, idx):
+        """
+        Compute homography matrix to transform image 2 to image 1.
+        idx: index to a subset of correspondences for homography computation.
+        Return a 3x3 matrix [h11, h12, h13; h21, h22, h23; h31, h32, h33].
+        """
+        # each correspondence gives two equations
+        nbEqns = len(idx) * 2;
+        A = np.matrix(np.zeros((nbEqns, 9)));
+        #b = np.zeros((nbEqns, 1));
+        for i in range(len(idx)):
+            p1 = self.locs1[idx[i]]; p1x = p1[0]; p1y = p1[1];
+            p2 = self.locs2[idx[i]]; p2x = p2[0]; p2y = p2[1];
+            A[2 * i, :] = [p2x, p2y, 1, 0, 0, 0, -p1x * p2x, -p1x * p2y, -p1x];
+            A[2 * i + 1, :] = [0, 0, 0, p2x, p2y, 1, -p1y * p2x, -p1y * p2y, -p1y];
+        # solve Ah = 0 using SVD
+        U, s, Vh = linalg.svd(A);
+        # h is the last column of V, or last row of Vh.
+        h = Vh[8, :];        
+        #print h
+        #print h        
+        #print A * h        
+        #print type(A)
+        return np.matrix(np.reshape(h, (3, 3), 'C')); # C means C matrix, row-major.        
+        
+    def ransac(self):
+        # take 4 random correspondences
+        size = 4;
+                
+        # RANSAC
+        iters = 200;
+        maxInliers = 0;
+        for i in range(iters):
+            idx = np.random.randint(0, len(self.locs1) - 1, size);
+            """
+            locs1 = [];
+            locs2 = [];
+            for j in idx:
+                locs1.append(self.locs1[j]);
+                locs2.append(self.locs2[j]);
+            """
+            H = self.__homography__(idx);
+            #print H;
+            
+            # check for consistency
+            mask = self.__checkConsistency__(H, 0.5);
+            newInliers = np.sum(mask);
+            if newInliers > maxInliers:
+                maxInliers = newInliers;
+                bestMask = mask;
+                
+        # recompute H with all inliers
+        locs1 = [];
+        locs2 = [];
+        for i in range(len(self.locs1)):
+            if bestMask[i] == 1:
+                locs1.append(self.locs1[i]);
+                locs2.append(self.locs2[i]);
+        # only store inliers  
+        self.locs1 = locs1;
+        self.locs2 = locs2;  
+        idx = range(len(self.locs1));            
+        H = self.__homography__(idx);        
+        print "Best homography: ", H
+        print "Inliers/Outliers: ", np.sum(bestMask), "/", len(bestMask) - np.sum(bestMask);
+                    
+    def __checkConsistency__(self, H, eps=1):
+        """
+        eps: the tolerance error (in pixel) of the homography estimation
+        
+        Return an array indicating inliers. 
+        0 : outlier. 1 : inlier.
+        """
+        # map from image2 to image1
+        eps2 = eps**2; # tolerance 2 pixels
+        mask = np.ones(len(self.locs1));
+        for i in range(len(self.locs1)):
+            #p2 = np.reshape(np.array([self.locs2[i][0], self.locs2[i][1], 1]), (3, 1));            
+            p2 = np.matrix([ [self.locs2[i][0]], [self.locs2[i][1]], [1] ]);
+            p1 = H * p2;
+            p1 = p1 / p1[2]; # convert back to non-homogeneous
+            
+            # compute the distance of p1 and original locs1
+            dist = (p1[0] - self.locs1[i][0])**2 + (p1[1] - self.locs1[i][1])**2;
+            if dist > eps2:
+                mask[i] = 0; # outlier
+
+            #print type(p2)
+            #print type(p1)
+            #print p2
+            #print p1
+            # 
+            #return
+        return mask
+    def show(self):        
+        scores = range(len(self.locs1));
+        sift.plot_matches_2(self.image1.pixels, self.image2.pixels, self.locs1, self.locs2, scores);
         
 class ImageMosaick(object):
     def __init__(self):
@@ -116,26 +219,41 @@ class ImageMosaick(object):
             imageIds = [];
             for j in range(i + 1, len(self.images)):
                 im = self.images[j];
+                # join two list -> use extend
                 features.extend(im.descriptors);
                 locations.extend(im.locations); # a list of array(4)
                 # image index of each features
                 imageIds.extend([im.id for m in range(len(im.descriptors))]);
             
             # insert all features into Kd-tree
-            kdtree = KdtreeCustom.KDTree(features, leafsize=64);
+            kdtree = KdtreeCustom.KDTree(features, leafsize=512);
            
             # for each feature find K nearest neighbor match
             knearest = 2; # minimum is 2. 1 works not well as it's a hard threshold.
             distRatio = 0.8;
+            
+            """
+            start = time.clock();
+            dists, idxs = kdtree.query(self.images[i].descriptors, knearest, eps=0.01, distance_upper_bound=0.3);
+            elapsed = time.clock() - start;
+            print "Query: ", elapsed
+            return;
+            """
+            
             for n in range(len(self.images[i].descriptors)):
-                #print n, "."
+                
                 ft = self.images[i].descriptors[n];
-                #print n, "-"
-                dist, idx = kdtree.query(ft, knearest);
+                
+                #start = time.clock();
+                dist, idx = kdtree.query(ft, knearest, eps=0.01, distance_upper_bound=0.1); # prune more? 
+                #elapsed = time.clock() - start;
+                #print "Query: ", elapsed
                 #print dist
                 
                 # not accept duplicated items
                 #idxUnique, indices = np.unique1d(idx, return_index=True);
+                
+                #start = time.clock();
                 if knearest > 1:
                     matchedImages = set();
                     # the difference between the best and second match should not be larger than 0.8
@@ -144,13 +262,17 @@ class ImageMosaick(object):
                             m = imageIds[idx[j]];
                             if m not in matchedImages:
                                 matchedImages.add(m);
+                                # add each element of a list to a list -> append
                                 self.match[i][m].locs1.append(self.images[i].locations[n]);
                                 self.match[i][m].locs2.append(locations[idx[j]]);          
                 else:
                     if dist < 0.35:
                         m = imageIds[idx];
                         self.match[i][m].locs1.append(self.images[i].locations[n]);
-                        self.match[i][m].locs2.append(locations[idx]);  
+                        self.match[i][m].locs2.append(locations[idx]);
+                        
+                #elapsed = time.clock() - start;
+                #print "Remaining: ", elapsed  
             #print match[n];
             #print [imageIds[match[n][j]] for j in range(knearest)];
         
@@ -158,24 +280,26 @@ class ImageMosaick(object):
         print "Kdtree: ", elapsed; 
         
         # recover H for each pair of images
-        # remove outliers using RANSAC
-        # 
+        # and use RANSAC to reject outliers
+        start = time.clock();
+        self.match[0][1].ransac();
+        elapsed = time.clock() - start;
+        print "RANSAC: ", elapsed; 
         
         
     def show(self):
         #[im.show() for im in self.images];
-        
         pylab.figure(0);
-        scores = range(len(self.match[0][1].locs1));
-        #print self.match[0][1].locs1;
-        #print self.match[0][1].locs1
-        sift.plot_matches_2(self.images[0].pixels, self.images[1].pixels, self.match[0][1].locs1, self.match[0][1].locs2, scores);
+        self.match[0][1].show();
         pylab.show();
         
 def main():
     imo = ImageMosaick();
     folder  = "./images";
-    images  = ["scene.pgm", "basmati.pgm"];
+    #images  = ["scene.pgm", "basmati.pgm"];
+    #images = ["PICT0013.pgm", "PICT0014.pgm"];
+    #images = ["PICT0014_800.pgm", "PICT0013_800.pgm"];
+    images = ["PICT0015_800.pgm", "PICT0014_800.pgm"];
     imo.mosaick([folder + "/" + image for image in images]);
     imo.show();
     
